fb44cf8626b08ed5e0315b98691d66863f396cc8
Alexander <alzahar@ukr.net>
2024-03-07 14:46:46 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyElement = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const txtColorOutput = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const currentColor = getRandomHexColor();
+  bodyElement.setAttribute('style', `background-color: ${currentColor}`);
+  txtColorOutput.innerHTML = currentColor;
+})
