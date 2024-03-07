34246d9a310cc530dca4995e9741cfa1fc329780
Alexander <alzahar@ukr.net>
2024-03-07 14:08:50 +0200

    fix: task-3

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ const nameInput = document.querySelector('#name-input');
 const outputElement = document.querySelector('#name-output');
 
 nameInput.addEventListener('input', (e) => {
-    const text = e.currentTarget.value.trim();
+    const text = e.target.value.trim();
     if (text.length) {
         outputElement.innerHTML = text;
     } else {
