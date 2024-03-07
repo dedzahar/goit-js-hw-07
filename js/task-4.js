d0fb9d7819e6b4927d72de2e5a1b213a0d519d3b
Alexander <alzahar@ukr.net>
2024-03-07 14:35:43 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formValues = [];
+    const formInputs = e.target.elements;
+    for (const input of formInputs) {
+        if (input.nodeName.toLowerCase() !== 'input') {
+            continue;
+        } else if (!input.value.trim().length) {
+            alert('All form fields must be filled in');
+        }
+        formValues[input.name] = input.value.trim();
+    }
+    e.target.reset();
 
+    console.log(formValues);
+})
