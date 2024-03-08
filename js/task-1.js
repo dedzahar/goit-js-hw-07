f8958020de61b46a754a0dbefab3c14a6698ef3c
Alexander <alzahar@ukr.net>
2024-03-08 12:04:44 +0200

    fix: task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,37 @@
+class Category {
+    #title;
+    #cnt;
+
+    set title(title) {
+        this.#title = title;
+    }
+
+    set cnt(cnt) {
+        this.#cnt = cnt;
+    }
+
+    get title() {
+        return this.#title;
+    }
+
+    get cnt() {
+        return this.#cnt;
+    }
+
+}
 const categories = document.querySelectorAll('.item');
 console.log('Number of categories: ' + categories.length);
 
 categories.forEach(category => {
-    const childs = category.children;
-    const item = [];
-    for (const child of childs) {
-        if (child.tagName.toLowerCase() == 'h2') {
-            item.name = child.innerHTML;
-        } else if (child.tagName.toLowerCase() == 'ul') {
+    const categoryChildren = category.children;
+    const item = new Category();
+    for (const child of categoryChildren) {
+        if (child.tagName == 'H2') {
+            item.title = child.innerHTML;
+        } else if (child.tagName == 'UL') {
             item.cnt = child.children.length;
         }
     }
-    console.log(`Category: ${item.name}`);
+    console.log(`Category: ${item.title}`);
     console.log(`Elements: ${item.cnt}`);
 });
